b77c6b29e72e60ac15db240640b0e9076ccccb2d
karansoni773 <156766722+karansoni773@users.noreply.github.com>
2024-07-28 10:53:47 +0530

    Add files via upload

---
+++ Whatsappmsg.py
@@ -0,0 +1,17 @@
+import pyautogui as pg
+import time
+
+# Giving Dealy to run program
+print("program will run after 5 second")
+time.sleep(5)
+print("running")
+
+# Note : after running the program immediately open whatsapp web then open the persons chat you want to send messages
+
+# For loop
+for i in range(100):
+    # writing the messages
+    pg.write("pick up the call")
+    time.sleep(0.5)
+    # Seding the messages by pressing enter
+    pg.press("Enter")
